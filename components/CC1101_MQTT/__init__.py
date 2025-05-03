71e72fc27e00adbccc504df4f9a4b9d32851355b
Laszlo Papai <13553984+laszlopapai@users.noreply.github.com>
2025-05-03 18:11:20 +0200

    pins

--- components/CC1101_MQTT/__init__.py
+++ components/CC1101_MQTT/__init__.py
@@ -1,6 +1,7 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import spi
+from esphome import pins
 from esphome.const import CONF_ID, CONF_PIN
 
 CONF_SS = "ss_pin"
