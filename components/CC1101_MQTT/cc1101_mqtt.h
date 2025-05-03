2ea31a66e00b2642ec45485cb225a9d6abb9676f
Laszlo Papai <13553984+laszlopapai@users.noreply.github.com>
2025-05-04 00:44:38 +0200

    pin

--- components/CC1101_MQTT/cc1101_mqtt.h
+++ components/CC1101_MQTT/cc1101_mqtt.h
@@ -3,14 +3,14 @@
 #include "cc1101.h"
 #include "esphome/core/component.h"
 #include "esphome/components/spi/spi.h"
-//#include "esphome/components/mqtt/custom_mqtt_device.h"
+#include "esphome/components/mqtt/custom_mqtt_device.h"
 
 namespace esphome {
 namespace cc1101 {
 
 class cc1101_mqtt
     : public Component
-//    , public spi::SPIDevice<spi::BIT_ORDER_MSB_FIRST, spi::CLOCK_POLARITY_LOW, spi::CLOCK_PHASE_LEADING, spi::DATA_RATE_200KHZ>
+    , public spi::SPIDevice<spi::BIT_ORDER_MSB_FIRST, spi::CLOCK_POLARITY_LOW, spi::CLOCK_PHASE_LEADING, spi::DATA_RATE_200KHZ>
 //    , public mqtt::CustomMQTTDevice
 {
 public:
