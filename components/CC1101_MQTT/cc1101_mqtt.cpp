1b205de5a3663c9728e045f41f262ff3141b5b3f
Laszlo Papai <13553984+laszlopapai@users.noreply.github.com>
2025-05-03 14:11:23 +0200

    test

--- components/CC1101_MQTT/cc1101_mqtt.cpp
+++ components/CC1101_MQTT/cc1101_mqtt.cpp
@@ -23,4 +23,10 @@ void cc1101_mqtt::loop() {
 
 void cc1101_mqtt::dump_config() {
   ESP_LOGCONFIG(TAG, "CC1101 SPI Configuration:");
+  ESP_LOGCONFIG(TAG, "SPI SCK: %d", this->m_sck);
+  ESP_LOGCONFIG(TAG, "SPI MISO: %d", this->m_miso);
+  ESP_LOGCONFIG(TAG, "SPI MOSI: %d", this->m_mosi);
+  ESP_LOGCONFIG(TAG, "SPI SS: %d", this->m_ss);
+  ESP_LOGCONFIG(TAG, "GDO0: %d", this->m_gdo0);
+  ESP_LOGCONFIG(TAG, "GDO2: %d", this->m_gdo2);
 }
