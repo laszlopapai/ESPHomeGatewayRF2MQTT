1fd4334f936f2d222b5e09774ed7de5d08abc972
Laszlo Papai <13553984+laszlopapai@users.noreply.github.com>
2025-05-14 14:01:09 +0200

    mqtt

--- components/CC1101_MQTT/cc1101_mqtt.h
+++ components/CC1101_MQTT/cc1101_mqtt.h
@@ -13,8 +13,8 @@ namespace esphome {
 namespace cc1101 {
 
 class cc1101_mqtt
-    : public Component
-    , public spi::SPIDevice<spi::BIT_ORDER_MSB_FIRST, spi::CLOCK_POLARITY_LOW, spi::CLOCK_PHASE_LEADING, spi::DATA_RATE_200KHZ>
+    : /*public Component
+    ,*/ public spi::SPIDevice<spi::BIT_ORDER_MSB_FIRST, spi::CLOCK_POLARITY_LOW, spi::CLOCK_PHASE_LEADING, spi::DATA_RATE_200KHZ>
     , public mqtt::MQTTComponent
 {
 public:
